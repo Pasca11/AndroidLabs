10e014502cb53bb8cbb1ac43de0a80c71073c2a0
Pasca11 <dr.marshall.work@gmail.com>
2024-09-19 15:46:25 +0300

    refactor

--- TaskManager/app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ TaskManager/app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.taskmanager
 
 import android.os.Bundle
@@ -39,7 +23,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskCompletedTheme
-import com.example.taskmanager.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
