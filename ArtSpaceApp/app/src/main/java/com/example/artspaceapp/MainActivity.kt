cdac75ff0b0dd7df22336f9df19d518f4c766a06
Pasca11 <dr.marshall.work@gmail.com>
2024-10-17 14:55:40 +0300

    display

--- ArtSpaceApp/app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ ArtSpaceApp/app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -17,8 +19,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspaceapp.ui.theme.ArtSpaceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -67,6 +73,56 @@ fun DisplayFooter(
     }
 }
 
+@Composable
+fun DisplayInfo(imageId: Int,modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement  = Arrangement.Center,
+        horizontalAlignment  = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+
+    ) {
+        Text(
+            fontSize = 23.sp,
+            fontWeight = FontWeight.Bold,
+            text = stringResource(id = GetTitle(imageId)),
+            modifier = modifier
+                .padding(10.dp)
+        )
+        Text(
+            text = stringResource(id = GetImageDescription(imageId)) ,
+            fontSize = 18.sp,
+            fontStyle = FontStyle.Italic,
+            modifier = modifier
+                .padding(bottom = 10.dp)
+        )
+    }
+}
+
+private fun GetImage(imageId: Int):Int{
+    return when(imageId){
+        1 -> R.drawable.pic1
+        2 -> R.drawable.pic2
+        3 -> R.drawable.pic2
+        else -> R.drawable.pic4
+    }
+}
+private fun GetImageDescription(imageId: Int):Int{
+    return when(imageId){
+        1 -> R.string.description_1
+        2 -> R.string.description_2
+        3 -> R.string.description_3
+        else -> R.string.description_4
+    }
+}
+private fun GetTitle(imageId: Int):Int{
+    return when(imageId){
+        1 -> R.string.title_1
+        2 -> R.string.title_2
+        3 -> R.string.title_3
+        else -> R.string.title_4
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
