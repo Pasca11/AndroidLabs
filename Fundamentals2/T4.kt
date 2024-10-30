90ec63063e135ab0f1ff8cc89449d424b8d1b5bc
Pasca11 <dr.marshall.work@gmail.com>
2024-10-30 23:51:36 +0300

    task4

---
+++ Fundamentals2/T4.kt
@@ -0,0 +1,13 @@
+data class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty
+)
+
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+
+fun main() {
+    print(Question(questionText="Quoth the raven ___", answer="nevermore", difficulty=Difficulty.MEDIUM))
+}
