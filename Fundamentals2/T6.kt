95693b094628ce3e400ba9ecdc9fa006552ee7a6
Pasca11 <dr.marshall.work@gmail.com>
2024-10-31 00:07:23 +0300

    ext

---
+++ Fundamentals2/T6.kt
@@ -0,0 +1,9 @@
+val Quiz.StudentProgress.progressText: String
+    get() = "${answered} of ${total} answered"
+
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
+    println(Quiz.progressText)
+}
