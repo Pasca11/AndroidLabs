3ecda6c6f3a7498914c8b4239a5950baa6d4d238
Pasca11 <dr.marshall.work@gmail.com>
2024-10-31 00:10:55 +0300

    interface

---
+++ Fundamentals2/T7.kt
@@ -0,0 +1,16 @@
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
+
+class Quiz {
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
+
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("▓") }
+        repeat(Quiz.total - Quiz.answered) { print("▒") }
+        println()
+        println(progressText)
+    }
+}
