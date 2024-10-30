f69e0e1787d02e5ec2e06361a32bd7bfc4ecd208
Pasca11 <dr.marshall.work@gmail.com>
2024-10-31 00:29:01 +0300

    sets

---
+++ Fundamentals2/C4.kt
@@ -0,0 +1,9 @@
+val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+
+println(solarSystem.size)
+
+solarSystem.add("Pluto")
+
+println(solarSystem.contains("Pluto"))
+
+solarSystem.remove("Pluto")
